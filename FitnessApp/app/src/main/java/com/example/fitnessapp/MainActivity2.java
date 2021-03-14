e15c04426cb4c3c5f56c507abff19b795f24720b
GF <marjie.fresnoza@gmail.com>
2021-03-14 23:06:25 +0800

    Fitness app

--- FitnessApp/app/src/main/java/com/example/fitnessapp/MainActivity2.java
+++ FitnessApp/app/src/main/java/com/example/fitnessapp/MainActivity2.java
@@ -2,7 +2,9 @@ package com.example.fitnessapp;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
@@ -12,17 +14,59 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
-public class MainActivity2 extends AppCompatActivity {
+public class MainActivity2 extends AppCompatActivity implements View.OnClickListener {
+
     ImageButton imghome;
+
+    public CardView card1;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
+        card1 = (CardView) findViewById(R.id.c1);
+
+
+
+
+        card1.setOnClickListener(this);
+
+
+
+
+
+
         //action bar title AND color
         getSupportActionBar().setTitle("Food");
         getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#ff7f50")));
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i;
+
+        switch (v.getId()){
+            case R.id.c1:
+                i = new Intent( this,Day1.class);
+                startActivity(i);
+                break;
+
+
+
+
+
+
+
+
+        }
+
 
     }
 }
