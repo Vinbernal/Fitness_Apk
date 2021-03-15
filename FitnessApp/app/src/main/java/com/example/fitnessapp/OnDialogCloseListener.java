5283746428434925c3d0eb6b07efe12f34b94c70
Gerald Siaron <79343015+Gsiaron@users.noreply.github.com>
2021-03-15 15:14:30 +0800

    Planner
    SetPlanner

---
+++ FitnessApp/app/src/main/java/com/example/fitnessapp/OnDialogCloseListener.java
@@ -0,0 +1,7 @@
+package com.example.fitnessapp;
+
+import android.content.DialogInterface;
+
+public interface OnDialogCloseListener {
+    void onDialogClose(DialogInterface dialogInterface);
+}
--- FitnessApp/app/src/main/java/com/example/fitnessapp/Planner.java
+++ FitnessApp/app/src/main/java/com/example/fitnessapp/Planner.java
@@ -1,7 +1,10 @@
 package com.example.fitnessapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
@@ -9,7 +12,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 
-public class Planner extends AppCompatActivity {
+import com.example.fitnessapp.Adapter.ToDoAdapter;
+import com.example.fitnessapp.Mode.ToDoMode;
+import com.example.fitnessapp.Utils.DataBasaHelper;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class Planner extends AppCompatActivity implements OnDialogCloseListener {
+    private RecyclerView mRecyclerView;
+    private FloatingActionButton fab;
+    private DataBasaHelper myDB;
+    private List<ToDoMode> mList;
+    private ToDoAdapter adapter;
 
     ImageButton imghome;
 
@@ -22,8 +40,41 @@ public class Planner extends AppCompatActivity {
         getSupportActionBar().setTitle("Planner");
         getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#ff7f50")));
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-//
 
+        mRecyclerView = findViewById(R.id.recyclerview);
+        fab = findViewById(R.id.fab);
+        myDB = new DataBasaHelper(Planner.this);
+        mList = new ArrayList<>();
+        adapter = new ToDoAdapter(myDB, Planner.this);
+
+        mList = myDB.getAllTask();
+        Collections.reverse(mList);
+        adapter.setTasks(mList);
+
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setAdapter(adapter);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AddNewTask.newInstance().show(getSupportFragmentManager(), AddNewTask.TAG);
+
+            }
+        });
+
+        }
+
+
+
+
+
+    @Override
+    public void onDialogClose(DialogInterface dialogInterface) {
+        mList = myDB.getAllTask();
+        Collections.reverse(mList);
+        adapter.setTasks(mList);
+        adapter.notifyDataSetChanged();
 
     }
 }
