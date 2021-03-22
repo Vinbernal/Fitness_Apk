d54fb6eed74f232255a8e609afa24155fe74f5d6
GF <marjie.fresnoza@gmail.com>
2021-03-22 16:35:58 +0800

    Fitness app

--- FitnessApp/app/src/main/java/com/example/fitnessapp/MainActivity2.java
+++ FitnessApp/app/src/main/java/com/example/fitnessapp/MainActivity2.java
@@ -10,10 +10,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
 import com.example.fitnessapp.dayfood.Day1;
+import com.example.fitnessapp.dayfood.Day2;
+import com.example.fitnessapp.dayfood.Day3;
+import com.example.fitnessapp.dayfood.Day4;
+import com.example.fitnessapp.dayfood.Day5;
 
 public class MainActivity2 extends AppCompatActivity implements View.OnClickListener {
 
-    public CardView card1;
+    public CardView card1, card2, card3, card4, card5;
+
 
 
 
@@ -23,12 +28,18 @@ public class MainActivity2 extends AppCompatActivity implements View.OnClickList
         setContentView(R.layout.activity_main2);
 
         card1 = (CardView) findViewById(R.id.c1);
-
+        card2 = (CardView) findViewById(R.id.c2);
+        card3 = (CardView) findViewById(R.id.c3);
+        card4 = (CardView) findViewById(R.id.c4);
+        card5 = (CardView) findViewById(R.id.c5);
 
 
 
         card1.setOnClickListener(this);
-
+        card2.setOnClickListener(this);
+        card3.setOnClickListener(this);
+        card4.setOnClickListener(this);
+        card5.setOnClickListener(this);
 
 
 
@@ -52,6 +63,23 @@ public class MainActivity2 extends AppCompatActivity implements View.OnClickList
                 i = new Intent( this, Day1.class);
                 startActivity(i);
                 break;
+            case R.id.c2:
+                i = new Intent( this, Day2.class);
+                startActivity(i);
+                break;
+            case R.id.c3:
+                i = new Intent( this, Day3.class);
+                startActivity(i);
+                break;
+            case R.id.c4:
+                i = new Intent( this, Day4.class);
+                startActivity(i);
+                break;
+            case R.id.c5:
+            i = new Intent( this, Day5.class);
+            startActivity(i);
+            break;
+
 
 
 
---
+++ FitnessApp/app/src/main/java/com/example/fitnessapp/dayfood/Day2.java
@@ -0,0 +1,23 @@
+package com.example.fitnessapp.dayfood;
+
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.fitnessapp.R;
+
+public class Day2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_day2);
+
+        getSupportActionBar().setTitle("Day 2 ");
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#ff7f50")));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+}
