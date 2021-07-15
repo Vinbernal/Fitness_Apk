381822865dbb93ddd399a25de4ba9e28a2706737
Marvin Bernal <77772098+Vinbernal@users.noreply.github.com>
2021-07-15 23:41:06 +0800

    3dotAboutus

--- FitnessApp/app/src/main/java/com/example/fitnessapp/MainActivity.java
+++ FitnessApp/app/src/main/java/com/example/fitnessapp/MainActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -73,14 +76,28 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(newact);
             }
         });
-        imgaboutus = (ImageButton) findViewById(R.id.imageButton);
-        imgaboutus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent newts1 = new Intent(MainActivity.this, aboutus.class);
-                startActivity(newts1);
-            }
-        });
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.item1){
+
+            Intent intent = new Intent(MainActivity.this,aboutus.class);
+            startActivity(intent);
+
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
